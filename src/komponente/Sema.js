52cd63506c91e950c8a51906bac933f26aac47d1
Anja327 <djulijana1999@gmail.com>
2022-06-15 22:26:39 +0200

    Komponenta Sema, programske seme definisane, select za izbor kanala, useState Hooks

---
+++ src/komponente/Sema.js
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+
+function Sema() {
+
+
+    var superStar = [
+        { id: 1, vreme: '08:00', naslov: "FILM: TOP GUN" },
+        { id: 2, vreme: '12:00', naslov: "SERIJA: DRŽAVNI POSAO" },
+        { id: 3, vreme: '15:00', naslov: "SERIJA: KLAN" },
+        { id: 4, vreme: '18:00', naslov: "FILM: ZABRANJENO KRALJEVSTVO" },
+        { id: 5, vreme: '21:00', naslov: "FILM: KRALJ PETAR " },
+        { id: 6, vreme: '00:00', naslov: "FILM: OTKAČENI PROFESOR" },
+        { id: 7, vreme: '02:00', naslov: "FILM: LIKVIDATOR" },
+        { id: 8, vreme: '05:00', naslov: "SERIJA: ZBORNICA" },
+    ]
+
+    var pinkFilm = [
+        { id: 1, vreme: '08:00', naslov: "FILM: DOKTOR LUDOSTI" },
+        { id: 2, vreme: '12:00', naslov: "FILM: ZAJEDNO" },
+        { id: 3, vreme: '15:00', naslov: "FILM: MEHANIZAM" },
+        { id: 4, vreme: '18:00', naslov: "FILM: VREME ČUDA" },
+        { id: 5, vreme: '21:00', naslov: "FILM: VOJNICI" },
+        { id: 6, vreme: '00:00', naslov: "FILM: HAJKA" },
+        { id: 7, vreme: '02:00', naslov: "FILM: VIRTUELNA STVARNOST" },
+        { id: 8, vreme: '05:00', naslov: "FILM: ŠEŠIR PROFESORA KOSTE VUJIĆA" },
+    ]
+
+    var hbo = [
+        { id: 1, vreme: '08:00', naslov: "FILM: ANGRY BIRDS" },
+        { id: 2, vreme: '12:00', naslov: "FILM: HOLIVUD NA SNIMANJU" },
+        { id: 3, vreme: '15:00', naslov: "FILM: PETI SET" },
+        { id: 4, vreme: '18:00', naslov: "FILM: AMERIČKA TURŠIJA" },
+        { id: 5, vreme: '21:00', naslov: "FILM: TENET" },
+        { id: 6, vreme: '00:00', naslov: "FILM: CRNO ZLATO" },
+        { id: 7, vreme: '02:00', naslov: "FILM: SRODNICI" },
+        { id: 8, vreme: '05:00', naslov: "FILM: HENKOK" },
+    ]
+
+    const [izbor, setIzbor] = useState();
+
+    function selectHandle(e) {
+        setIzbor(e.target.value);
+    }
+
+    return (
+        <div className="divsema">
+            <select id="izborprogramaselect" onChange={selectHandle}>
+                <option>Izaberi</option>
+                <option value={"superStar"}>PinkFilm</option>
+                <option value={"pinkFilm"}>SUPERSTAR</option>
+                <option value={"hbo"}>HBO</option>
+            </select>
+        </div>
+    )
+}
+
+export default Sema;
--- src/strane/Home.js
+++ src/strane/Home.js
@@ -1,9 +1,11 @@
+import Sema from "../komponente/Sema";
 
 function Home() {
 
     return (
         <div className="divhome">
             <h1 id="tv-naslov">TV PROGRAMSKA ŠEMA</h1>
+            <Sema />
         </div>
     )
 
